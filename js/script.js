183a57e54f4006d017f149cbee9854b089f64744
Giuseppealfosnisi <costanza.matteo@ipsravizza.edu.it>
2026-03-28 14:50:35 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,8 @@
-window.addEventListener("scroll", function () {
-const header = document.querySelector("header");
+window.addEventListener("scroll", () => {
+const nav = document.querySelector(".navbar");
 if (window.scrollY > 50) {
-header.style.background = "#1f3d2b";
+nav.style.background = "#1f3d2b";
 } else {
-header.style.background = "rgba(0,0,0,0.6)";
+nav.style.background = "rgba(0,0,0,0.6)";
 }
-});
-
-document.querySelectorAll("a").forEach(anchor => {
-anchor.addEventListener("click", function (e) {
-const target = this.getAttribute("href");
-
-```
-if (target.startsWith("#") && document.querySelector(target)) {
-  e.preventDefault();
-  document.querySelector(target).scrollIntoView({
-    behavior: "smooth"
-  });
-}
-```
-
-});
 });
