542287badf35d0202e806169bcdb58ae2002418d
Giuseppealfosnisi <costanza.matteo@ipsravizza.edu.it>
2026-03-28 15:36:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-// Scroll effetto navbar (opzionale)
 window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
+const nav = document.querySelector(".navbar");
 
-    if (window.scrollY > 50) {
-        nav.style.background = "rgba(0,0,0,0.8)";
-    } else {
-        nav.style.background = "linear-gradient(to bottom, rgba(0,0,0,0.6), transparent)";
-    }
+if (window.scrollY > 50) {
+nav.style.background = "rgba(0,0,0,0.85)";
+} else {
+nav.style.background = "linear-gradient(to bottom, rgba(0,0,0,0.6), transparent)";
+}
 });
