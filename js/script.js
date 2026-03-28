285415012d7204e6ccd0487357b905b452c33638
Giuseppealfosnisi <costanza.matteo@ipsravizza.edu.it>
2026-03-28 14:55:04 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 window.addEventListener("scroll", () => {
 const nav = document.querySelector(".navbar");
+
 if (window.scrollY > 50) {
 nav.style.background = "#1f3d2b";
 } else {
-nav.style.background = "rgba(0,0,0,0.6)";
+nav.style.background = "rgba(0,0,0,0.7)";
 }
 });
