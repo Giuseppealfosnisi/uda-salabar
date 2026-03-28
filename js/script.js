fb2c5f077ee35f27be30dace95a71469718a74f8
Giuseppealfosnisi <costanza.matteo@ipsravizza.edu.it>
2026-03-28 16:32:18 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-window.addEventListener("scroll", () => {
-const nav = document.querySelector(".navbar");
-
-if (window.scrollY > 50) {
-nav.style.background = "rgba(0,0,0,0.9)";
-} else {
-nav.style.background = "rgba(0,0,0,0.6)";
-}
-});
+console.log("JS collegato");
