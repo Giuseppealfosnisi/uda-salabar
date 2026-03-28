d104fae7b85fff41f1b80317a8eb1b25acac17db
Giuseppealfosnisi <costanza.matteo@ipsravizza.edu.it>
2026-03-28 15:31:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log("Sito attivo");
+// Scroll effetto navbar (opzionale)
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(0,0,0,0.8)";
+    } else {
+        nav.style.background = "linear-gradient(to bottom, rgba(0,0,0,0.6), transparent)";
+    }
+});
